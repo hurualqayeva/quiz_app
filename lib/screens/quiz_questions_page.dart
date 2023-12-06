64569b6c85b8f2cba4ce4602691e5b2db7a44cb4
hurualqayeva <hurualqayeva@gmail.com>
2023-12-07 02:05:59 +0400

    updated

--- lib/screens/quiz_questions_page.dart
+++ lib/screens/quiz_questions_page.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:quiz_app/models/model.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:quiz_app/screens/quiz_result_page.dart';
 
-class QuizState extends ChangeNotifier {
+class QuizQuestionsPage extends StatefulWidget {
+  final String selectedCategory;
+  final String selectedDifficulty;
+  final String selectedType;
+  final int numberOfQuestions;
+
+  QuizQuestionsPage({
+    required this.selectedCategory,
+    required this.selectedDifficulty,
+    required this.selectedType,
+    required this.numberOfQuestions,
+  });
+
+  @override
+  _QuizQuestionsPageState createState() => _QuizQuestionsPageState();
+}
+
+class _QuizQuestionsPageState extends State<QuizQuestionsPage> {
   late List<QuizQuestion> quizQuestions;
   late int currentQuestionIndex;
   late Map<int, String?> selectedAnswers;
@@ -9,66 +31,74 @@ class QuizState extends ChangeNotifier {
   late int timerSeconds;
   late double timerProgress;
 
-  QuizState()
-      : quizQuestions = [],
-        currentQuestionIndex = 0,
-        selectedAnswers = {},
-        timerSeconds = 60,
-        timerProgress = 1.0;
+  @override
+  void initState() {
+    super.initState();
+    quizQuestions = [];
+    currentQuestionIndex = 0;
+    selectedAnswers = {};
+    timerSeconds = 60;
+    timerProgress = 1.0;
+    fetchQuizQuestions();
+    startTimer();
+  }
 
   void startTimer() {
     timer = Timer.periodic(Duration(seconds: 1), (Timer t) {
-      if (timerSeconds > 0) {
-        timerSeconds--;
-        timerProgress = timerSeconds / 60.0;
-      } else {
-        goToNextQuestion();
-      }
-      notifyListeners();
+      setState(() {
+        if (timerSeconds > 0) {
+          timerSeconds--;
+          timerProgress = timerSeconds / 60.0;
+        } else {
+          goToNextQuestion();
+        }
+      });
     });
   }
 
-  Future<void> fetchQuizQuestions(int numberOfQuestions, String selectedCategory, String selectedDifficulty, String selectedType) async {
-    final categoryParam = selectedCategory.isNotEmpty ? '&category=$selectedCategory' : '';
-    final difficultyParam = selectedDifficulty == 'any' ? '' : '&difficulty=$selectedDifficulty';
-    final typeParam = selectedType == 'any' ? '' : '&type=$selectedType';
+  Future<void> fetchQuizQuestions() async {
+    final categoryParam = widget.selectedCategory.isNotEmpty ? '&category=${widget.selectedCategory}' : '';
+    final difficultyParam = widget.selectedDifficulty == 'any' ? '' : '&difficulty=${widget.selectedDifficulty}';
+    final typeParam = widget.selectedType == 'any' ? '' : '&type=${widget.selectedType}';
 
     final response = await http.get(Uri.parse(
-        'https://opentdb.com/api.php?amount=$numberOfQuestions$categoryParam$difficultyParam$typeParam'));
+        'https://opentdb.com/api.php?amount=${widget.numberOfQuestions}$categoryParam$difficultyParam$typeParam'));
     final data = json.decode(response.body);
 
-    quizQuestions = (data['results'] as List)
-        .map((questionData) => QuizQuestion(
-      question: questionData['question'],
-      options: List<String>.from(questionData['incorrect_answers']) +
-          [questionData['correct_answer']],
-      correctAnswer: questionData['correct_answer'],
-    ))
-        .toList();
-    notifyListeners();
+    setState(() {
+      quizQuestions = (data['results'] as List)
+          .map((questionData) => QuizQuestion(
+                question: questionData['question'],
+                options: List<String>.from(questionData['incorrect_answers']) +
+                    [questionData['correct_answer']],
+                correctAnswer: questionData['correct_answer'],
+              ))
+          .toList();
+    });
   }
 
-  void goToNextQuestion(int numberOfQuestions) {
-    if (currentQuestionIndex < numberOfQuestions - 1) {
-      currentQuestionIndex++;
-      timerSeconds = 60;
-      timerProgress = 1.0;
+  void goToNextQuestion() {
+    if (currentQuestionIndex < widget.numberOfQuestions - 1) {
+      setState(() {
+        currentQuestionIndex++;
+        timerSeconds = 60;
+        timerProgress = 1.0;
+      });
     } else {
       timer.cancel();
-      showQuizResult(numberOfQuestions);
+      showQuizResult();
     }
-    notifyListeners();
   }
 
-  void showQuizResult(BuildContext context, int numberOfQuestions) {
-    int correctCount = calculateCorrectAnswers(numberOfQuestions);
+  void showQuizResult() {
+    int correctCount = calculateCorrectAnswers();
 
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text('Quiz Result'),
-          content: Text('You answered $correctCount out of $numberOfQuestions questions correctly.'),
+          content: Text('You answered $correctCount out of ${widget.numberOfQuestions} questions correctly.'),
           actions: <Widget>[
             TextButton(
               onPressed: () {
@@ -98,125 +128,124 @@ class QuizState extends ChangeNotifier {
     );
   }
 
-  int calculateCorrectAnswers(int numberOfQuestions) {
+  int calculateCorrectAnswers() {
     int correctCount = 0;
-    for (int i = 0; i < numberOfQuestions; i++) {
+    for (int i = 0; i < widget.numberOfQuestions; i++) {
       if (selectedAnswers.containsKey(i) && selectedAnswers[i] == quizQuestions[i].correctAnswer) {
         correctCount++;
       }
     }
     return correctCount;
   }
-}
-
-class QuizQuestionsPage extends StatefulWidget {
-  final String selectedCategory;
-  final String selectedDifficulty;
-  final String selectedType;
-  final int numberOfQuestions;
-
-  QuizQuestionsPage({
-    required this.selectedCategory,
-    required this.selectedDifficulty,
-    required this.selectedType,
-    required this.numberOfQuestions,
-  });
 
   @override
-  _QuizQuestionsPageState createState() => _QuizQuestionsPageState();
-}
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
 
-class _QuizQuestionsPageState extends State<QuizQuestionsPage> {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => QuizState(),
-      builder: (context, child) {
-        final quizState = Provider.of<QuizState>(context);
+    if (quizQuestions.isEmpty) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Quiz Questions'),
+        ),
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
 
-        if (quizState.quizQuestions.isEmpty) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Quiz Questions'),
-            ),
-            body: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        }
-
-        return Scaffold(
-          body: Container(
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 37, 55, 83),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 37, 55, 83),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Question ${currentQuestionIndex + 1} of ${widget.numberOfQuestions}',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+              ),
+              SizedBox(height: 8),
+              LinearProgressIndicator(
+                value: timerProgress,
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+                backgroundColor: Colors.grey,
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Time Remaining: ${timerSeconds}s',
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+              SizedBox(height: 16),
+              Html(
+                data: quizQuestions[currentQuestionIndex].question,
+                style: {
+                  "html": Style(
+                    color: Colors.white,
+                    fontSize: FontSize(18),
+                  ),
+                },
+              ),
+              SizedBox(height: 16),
+              Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Question ${quizState.currentQuestionIndex + 1} of ${widget.numberOfQuestions}',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
-                  ),
-                  SizedBox(height: 8),
-                  LinearProgressIndicator(
-                    value: quizState.timerProgress,
-                    valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
-                    backgroundColor: Colors.grey,
-                  ),
-                  SizedBox(height: 16),
-                  Text(
-                    'Time Remaining: ${quizState.timerSeconds}s',
-                    style: TextStyle(fontSize: 16, color: Colors.white),
-                  ),
-                  SizedBox(height: 16),
-                  Html(
-                    data: quizState.quizQuestions[quizState.currentQuestionIndex].question,
-                    style: {
-                      "html": Style(
-                        color: Colors.white,
-                        fontSize: FontSize(18),
-                      ),
-                    },
-                  ),
-                  SizedBox(height: 16),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: quizState.quizQuestions[quizState.currentQuestionIndex].options.map((option) {
-                      bool isCorrect = option == quizState.quizQuestions[quizState.currentQuestionIndex].correctAnswer;
-                      bool isSelected = quizState.selectedAnswers[quizState.currentQuestionIndex] == option;
+                children: quizQuestions[currentQuestionIndex].options.map((option) {
+                  bool isCorrect = option == quizQuestions[currentQuestionIndex].correctAnswer;
+                  bool isSelected = selectedAnswers[currentQuestionIndex] == option;
 
-                      return RadioListTile<String>(
-                        title: Html(
-                          data: option,
-                          style: {
-                            "html": Style(
-                              color: Colors.white,
-                            ),
-                          },
-                        ),
-                        activeColor: Colors.blue,
-                        value: option,
-                        groupValue: quizState.selectedAnswers[quizState.currentQuestionIndex],
-                        onChanged: (String? value) {
-                          quizState.selectedAnswers[quizState.currentQuestionIndex] = value;
-                          quizState.notifyListeners();
+                  return MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: RadioListTile<String>(
+                      title: Html(
+                        data: option,
+                        style: {
+                          "html": Style(
+                            color: Colors.white,
+                          ),
                         },
-                      );
-                    }).toList(),
-                  ),
-                  SizedBox(height: 16),
-                  ElevatedButton(
-                    child: Text(
-                      'Next Question',
-                      style: TextStyle(fontSize: 18, color: Colors.white),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.blue,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0),
                       ),
+                      activeColor: Colors.blue,
+                      value: option,
+                      groupValue: selectedAnswers[currentQuestionIndex],
+                      onChanged: (String? value) {
+                        setState(() {
+                          selectedAnswers[currentQuestionIndex] = value;
+                        });
+                      },
+                      tileColor: isSelected
+                          ? (isCorrect ? Colors.green : Colors.red)
+                          : Colors.transparent,
+                      hoverColor: isCorrect ? Colors.green.withOpacity(0.5) : Colors.red.withOpacity(0.5),
                     ),
-                    onPressed: () {
-                      quizState.goToNextQuestion(widget.numberOfQuestions);
+                  );
+                }).toList(),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                child: Text(
+                  'Next Question',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+                onPressed: () {
+                  goToNextQuestion();
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
